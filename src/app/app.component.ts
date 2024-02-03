4896ec1678c3b9d8ed8b321952fcb198f7b08498
anastassiyakim <kim.anastassiya.03@gmail.com>
2024-02-03 16:50:11 -0500

    Header(without logo)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
