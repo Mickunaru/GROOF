1fbd7fa867229166094fc7aa2b6c95683b06961d
GitHubYanis <yanis21p@gmail.com>
2024-02-03 22:38:35 -0500

    roof dimensions visualization

--- src/app/services/forms-data.service.ts
+++ src/app/services/forms-data.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormsDataService {
+  height: BehaviorSubject<number> = new BehaviorSubject(0);
+  width: BehaviorSubject<number> = new BehaviorSubject(0);
+  xValue: BehaviorSubject<number> = new BehaviorSubject(0);
+  yValue: BehaviorSubject<number> = new BehaviorSubject(0);
+  
+  // constructor() { }
 
-  constructor() { }
+  setValues(h: number, w: number, x: number, y: number) {
+    this.height.next(h);
+    this.width.next(w);
+    this.xValue.next(x);
+    this.yValue.next(y);
+  }
 }
