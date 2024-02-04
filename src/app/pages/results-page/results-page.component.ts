f152df671be95f865da3a5430539f145ce8e38bc
GitHubYanis <yanis21p@gmail.com>
2024-02-04 10:37:52 -0500

    fixed css

--- src/app/pages/results-page/results-page.component.ts
+++ src/app/pages/results-page/results-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { FormComponent } from "../../components/form/form.component";
 import { RouterLink } from '@angular/router';
+import { DynamicGridReadOnlyComponent } from '../../components/dynamic-grid-read-only/dynamic-grid-read-only.component';
 
 @Component({
     selector: 'app-results-page',
     standalone: true,
     templateUrl: './results-page.component.html',
     styleUrl: './results-page.component.scss',
-    imports: [FormComponent, RouterLink]
+    imports: [FormComponent, RouterLink, DynamicGridReadOnlyComponent]
 })
 export class ResultsPageComponent {
 
